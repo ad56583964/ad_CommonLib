0c103d61131337d49a7314e67ad6ad0e5bd9d21e
ad56583964 <1176949486@qq.com>
2021-02-12 08:48:39 +0800

    :pencil: 添加f4 drv引用和外部引用 header

---
+++ lib_drv/stm32f4/_interface_stm32f4.h
@@ -0,0 +1,15 @@
+/*
+ * _stm32f4_interface.h
+ *
+ *  Created on: 2021年2月12日
+ *      Author: ad565
+ */
+
+#ifndef LIB_DRV_STM32F4__INTERFACE_STM32F4_H_
+#define LIB_DRV_STM32F4__INTERFACE_STM32F4_H_
+
+#include "stm32f4xx_hal.h"
+
+
+
+#endif /* LIB_DRV_STM32F4__INTERFACE_STM32F4_H_ */
---
+++ lib_drv/stm32f4/ad_stm32f4_hal.h
@@ -0,0 +1,15 @@
+/*
+ * ad_stm32f4_hal.h
+ *
+ *  Created on: 2021年2月12日
+ *      Author: ad565
+ */
+
+#ifndef LIB_DRV_STM32F4_AD_STM32F4_HAL_H_
+#define LIB_DRV_STM32F4_AD_STM32F4_HAL_H_
+
+
+
+
+
+#endif /* LIB_DRV_STM32F4_AD_STM32F4_HAL_H_ */
