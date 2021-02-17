00944d3812179c331b20f6198aec36b5a978f2a8
ad56583964 <1176949486@qq.com>
2021-02-17 22:40:17 +0800

    导入as5048驱动

---
+++ lib_srv/AS5048A/Inc/ad_as5048a.h
@@ -0,0 +1,17 @@
+/*
+ * ad_as5048a.h
+ *
+ *  Created on: Feb 17, 2021
+ *      Author: ad565
+ */
+
+#ifndef LIB_SRV_AS5048A_INC_AD_AS5048A_H_
+#define LIB_SRV_AS5048A_INC_AD_AS5048A_H_
+
+//引用https://github.com/Naxca/STM32F103_AS5048A/tree/master/USER
+
+
+
+
+
+#endif /* LIB_SRV_AS5048A_INC_AD_AS5048A_H_ */
